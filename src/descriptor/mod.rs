9bbd931bb7ac724b505c794c46e541eb60a4b70f
Andrew Poelstra <apoelstra@wpsoftware.net>
2019-03-23 00:22:04 +0000

    descriptor: fill out `script_pubkey` function

--- src/descriptor/mod.rs
+++ src/descriptor/mod.rs
@@ -23,9 +23,8 @@
 //! BIP32 paths, pay-to-contract instructions, etc.
 //!
 
-use bitcoin::blockdata::script::{self ,Script};
-use bitcoin::blockdata::transaction::SigHashType;
-use bitcoin::util::key::PublicKey;
+use bitcoin::{self, PublicKey, Script, SigHashType};
+use bitcoin::blockdata::script;
 use bitcoin_hashes::sha256;
 use secp256k1;
 use std::fmt;
@@ -38,10 +37,8 @@ use Error;
 /// Script descriptor
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Descriptor<P> {
-    /// A raw scriptpubkey
+    /// A raw scriptpubkey (including pay-to-pubkey)
     Bare(Miniscript<P>),
-    /// Pay-to-PubKey
-    Pk(P),
     /// Pay-to-PubKey-Hash
     Pkh(P),
     /// Pay-to-Witness-PubKey-Hash
@@ -64,9 +61,6 @@ impl<P> Descriptor<P> {
         match *self {
             Descriptor::Bare(ref descript) => {
                 Ok(Descriptor::Bare(descript.translate(translatefn)?))
-            }
-            Descriptor::Pk(ref pk) => {
-                translatefn(pk).map(Descriptor::Pk)
             }
             Descriptor::Pkh(ref pk) => {
                 translatefn(pk).map(Descriptor::Pkh)
@@ -95,7 +89,21 @@ impl Descriptor<PublicKey> {
     pub fn script_pubkey(&self) -> Script {
         match *self {
             Descriptor::Bare(ref d) => d.serialize(),
-            _ => unimplemented!()
+            Descriptor::Pkh(ref pk) => {
+                let addr = bitcoin::Address::p2pkh(pk, bitcoin::Network::Bitcoin);
+                addr.script_pubkey()
+            },
+            Descriptor::Wpkh(ref pk) => {
+                let addr = bitcoin::Address::p2wpkh(pk, bitcoin::Network::Bitcoin);
+                addr.script_pubkey()
+            },
+            Descriptor::ShWpkh(ref pk) => {
+                let addr = bitcoin::Address::p2shwpkh(pk, bitcoin::Network::Bitcoin);
+                addr.script_pubkey()
+            },
+            Descriptor::Sh(ref miniscript) => miniscript.serialize().to_p2sh(),
+            Descriptor::Wsh(ref miniscript) => miniscript.serialize().to_v0_p2wsh(),
+            Descriptor::ShWsh(ref miniscript) => miniscript.serialize().to_v0_p2wsh().to_p2sh(),
         }
     }
 }
@@ -103,8 +111,12 @@ impl Descriptor<PublicKey> {
 impl<P: ToString> Descriptor<P> {
     /// Attempts to produce a satisfying witness or scriptSig, as the case may be,
     /// for the descriptor
-    pub fn satisfy<F, H>(&self, keyfn: Option<&F>, hashfn: Option<&H>, age: u32)
-        -> Result<Vec<Vec<u8>>, Error>
+    pub fn satisfy<F, H>(
+        &self,
+        keyfn: Option<&F>,
+        hashfn: Option<&H>,
+        age: u32,
+    ) -> Result<Vec<Vec<u8>>, Error>
         where F: Fn(&P) -> Option<(secp256k1::Signature, Option<SigHashType>)>,
               H: Fn(sha256::Hash) -> Option<[u8; 32]>
     {
@@ -150,6 +162,10 @@ impl<P: FromStr> expression::FromTree for Descriptor<P>
                         let sub = Miniscript::from_tree(&newtop.args[0])?;
                         Ok(Descriptor::ShWsh(sub))
                     }
+                    ("wpkh", 1) => expression::terminal(
+                        &newtop.args[0],
+                        |pk| P::from_str(pk).map(Descriptor::ShWpkh)
+                    ),
                     _ => {
                         let sub = Miniscript::from_tree(&top.args[0])?;
                         Ok(Descriptor::Sh(sub))
@@ -186,7 +202,6 @@ impl <P: fmt::Debug> fmt::Debug for Descriptor<P> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Descriptor::Bare(ref sub) => write!(f, "{:?}", sub),
-            Descriptor::Pk(ref p) => write!(f, "pk({:?})", p),
             Descriptor::Pkh(ref p) => write!(f, "pkh({:?})", p),
             Descriptor::Wpkh(ref p) => write!(f, "wpkh({:?})", p),
             Descriptor::ShWpkh(ref p) => write!(f, "sh(wpkh({:?}))", p),
@@ -201,7 +216,6 @@ impl <P: fmt::Display> fmt::Display for Descriptor<P> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Descriptor::Bare(ref sub) => write!(f, "{}", sub),
-            Descriptor::Pk(ref p) => write!(f, "pk({})", p),
             Descriptor::Pkh(ref p) => write!(f, "pkh({})", p),
             Descriptor::Wpkh(ref p) => write!(f, "wpkh({})", p),
             Descriptor::ShWpkh(ref p) => write!(f, "sh(wpkh({}))", p),
@@ -214,7 +228,11 @@ impl <P: fmt::Display> fmt::Display for Descriptor<P> {
 
 #[cfg(test)]
 mod tests {
-    use bitcoin::util::key::PublicKey;
+    use bitcoin::{self, PublicKey};
+    use bitcoin::blockdata::{opcodes, script};
+    use bitcoin_hashes::{hash160, sha256};
+    use bitcoin_hashes::hex::FromHex;
+
     use std::str::FromStr;
 
     use Descriptor;
@@ -227,6 +245,117 @@ mod tests {
         assert!(Descriptor::<PublicKey>::from_str("pk()").is_err());
 
         assert!(Descriptor::<PublicKey>::from_str("pk(020000000000000000000000000000000000000000000000000000000000000002)").is_ok());
+    }
+
+    #[test]
+    pub fn script_pubkey() {
+        let bare = Descriptor::<PublicKey>::from_str(
+            "time(1000)"
+        ).unwrap();
+        assert_eq!(
+            bare.script_pubkey(),
+            bitcoin::Script::from(vec![0x02, 0xe8, 0x03, 0xb2]),
+        );
+
+        let pk = Descriptor::<PublicKey>::from_str(
+            "pk(020000000000000000000000000000000000000000000000000000000000000002)"
+        ).unwrap();
+        assert_eq!(
+            pk.script_pubkey(),
+            bitcoin::Script::from(vec![
+                0x21,
+                0x02,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
+                0xac,
+            ]),
+        );
+
+        let pkh = Descriptor::<PublicKey>::from_str(
+            "pkh(020000000000000000000000000000000000000000000000000000000000000002)"
+        ).unwrap();
+        assert_eq!(
+            pkh.script_pubkey(),
+            script::Builder::new()
+                .push_opcode(opcodes::all::OP_DUP)
+                .push_opcode(opcodes::all::OP_HASH160)
+                .push_slice(&hash160::Hash::from_hex(
+                    "84e9ed95a38613f0527ff685a9928abe2d4754d4",
+                ).unwrap()[..])
+                .push_opcode(opcodes::all::OP_EQUALVERIFY)
+                .push_opcode(opcodes::all::OP_CHECKSIG)
+                .into_script(),
+        );
+
+        let wpkh = Descriptor::<PublicKey>::from_str(
+            "wpkh(020000000000000000000000000000000000000000000000000000000000000002)"
+        ).unwrap();
+        assert_eq!(
+            wpkh.script_pubkey(),
+            script::Builder::new()
+                .push_opcode(opcodes::all::OP_PUSHBYTES_0)
+                .push_slice(&hash160::Hash::from_hex(
+                    "84e9ed95a38613f0527ff685a9928abe2d4754d4",
+                ).unwrap()[..])
+                .into_script(),
+        );
+
+        let shwpkh = Descriptor::<PublicKey>::from_str(
+            "sh(wpkh(020000000000000000000000000000000000000000000000000000000000000002))"
+        ).unwrap();
+        assert_eq!(
+            shwpkh.script_pubkey(),
+            script::Builder::new()
+                .push_opcode(opcodes::all::OP_HASH160)
+                .push_slice(&hash160::Hash::from_hex(
+                    "f1c3b9a431134cb90a500ec06e0067cfa9b8bba7",
+                ).unwrap()[..])
+                .push_opcode(opcodes::all::OP_EQUAL)
+                .into_script(),
+        );
+
+        let sh = Descriptor::<PublicKey>::from_str(
+            "sh(pk(020000000000000000000000000000000000000000000000000000000000000002))"
+        ).unwrap();
+        assert_eq!(
+            sh.script_pubkey(),
+            script::Builder::new()
+                .push_opcode(opcodes::all::OP_HASH160)
+                .push_slice(&hash160::Hash::from_hex(
+                    "aa5282151694d3f2f32ace7d00ad38f927a33ac8",
+                ).unwrap()[..])
+                .push_opcode(opcodes::all::OP_EQUAL)
+                .into_script(),
+        );
+
+        let wsh = Descriptor::<PublicKey>::from_str(
+            "wsh(pk(020000000000000000000000000000000000000000000000000000000000000002))"
+        ).unwrap();
+        assert_eq!(
+            wsh.script_pubkey(),
+            script::Builder::new()
+                .push_opcode(opcodes::all::OP_PUSHBYTES_0)
+                .push_slice(&sha256::Hash::from_hex(
+                    "f9379edc8983152dc781747830075bd53896e4b0ce5bff73777fd77d124ba085",
+                ).unwrap()[..])
+                .into_script(),
+        );
+
+        let shwsh = Descriptor::<PublicKey>::from_str(
+            "sh(wsh(pk(020000000000000000000000000000000000000000000000000000000000000002)))"
+        ).unwrap();
+        assert_eq!(
+            shwsh.script_pubkey(),
+            script::Builder::new()
+                .push_opcode(opcodes::all::OP_HASH160)
+                .push_slice(&hash160::Hash::from_hex(
+                    "4bec5d7feeed99e1d0a23fe32a4afe126a7ff07e",
+                ).unwrap()[..])
+                .push_opcode(opcodes::all::OP_EQUAL)
+                .into_script(),
+        );
     }
 }
 
