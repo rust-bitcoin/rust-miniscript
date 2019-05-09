65b27ba9957b2ebffc48e0b969a0b1eb6ab2297c
joemphilips <joemphilips@gmail.com>
2019-05-09 17:46:20 +0900

    assert when getting script size of the Threshold

--- src/miniscript/astelem.rs
+++ src/miniscript/astelem.rs
@@ -992,10 +992,13 @@ impl<P: ToPublicKey> AstElem<P> {
             AstElem::OrNotif(ref left, ref right) => left.script_size() +
                 right.script_size() + 3,
             AstElem::Thresh(k, ref subs) |
-            AstElem::ThreshV(k, ref subs) => 1 +
+            AstElem::ThreshV(k, ref subs) => {
+                assert!(!subs.is_empty(), "Threshold can not have empty sub expressions");
+                1 +
                 script_num_size(k) +
                 subs.iter().map(|s| s.script_size()).sum::<usize>() +
                 subs.len() - 1
+            }
         }
     }
 
