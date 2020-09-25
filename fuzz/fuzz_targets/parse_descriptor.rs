28158a399978dc0c69ab1258ed0b4025d305c615
Antoine Poinsot <darosior@protonmail.com>
2020-09-25 19:26:58 +0200

    fuzz: add a fuzz target for descriptor parsing
    Co-Authored-By: Andrew Poelstra <apoelstra@wpsoftware.net>
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

---
+++ fuzz/fuzz_targets/parse_descriptor.rs
@@ -0,0 +1,33 @@
+extern crate miniscript;
+
+use miniscript::descriptor::DescriptorPublicKey;
+use std::str::FromStr;
+
+fn do_test(data: &[u8]) {
+    let data_str = String::from_utf8_lossy(data);
+    if let Ok(dpk) = DescriptorPublicKey::from_str(&data_str) {
+        let output = dpk.to_string();
+        assert_eq!(data_str.to_lowercase(), output.to_lowercase());
+    }
+}
+
+#[cfg(feature = "afl")]
+extern crate afl;
+#[cfg(feature = "afl")]
+fn main() {
+    afl::read_stdio_bytes(|data| {
+        do_test(&data);
+    });
+}
+
+#[cfg(feature = "honggfuzz")]
+#[macro_use]
+extern crate honggfuzz;
+#[cfg(feature = "honggfuzz")]
+fn main() {
+    loop {
+        fuzz!(|data| {
+            do_test(data);
+        });
+    }
+}
