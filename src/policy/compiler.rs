cc404a7e6dc21eb1311081fd8c95551818974a92
Antoine Poinsot <darosior@protonmail.com>
2020-12-10 15:58:13 +0100

    compiler: test opcode limit and key duplication
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- src/policy/compiler.rs
+++ src/policy/compiler.rs
@@ -1165,7 +1165,7 @@ mod tests {
     use std::str::FromStr;
     use std::string::String;
 
-    use miniscript::{satisfy, Segwitv0};
+    use miniscript::{satisfy, Legacy, Segwitv0};
     use policy::Liftable;
     use script_num_size;
     use BitcoinSig;
@@ -1512,6 +1512,54 @@ mod tests {
             Err(CompilerError::LimitsExceeded),
             "Compilation succeeded with '{:?}' elements",
             n_elements,
+        );
+    }
+
+    #[test]
+    fn shared_limits() {
+        // Test the maximum number of OPs with a 67-of-68 multisig
+        let (keys, _) = pubkeys_and_a_sig(68);
+        let keys: Vec<Concrete<bitcoin::PublicKey>> =
+            keys.iter().map(|pubkey| Concrete::Key(*pubkey)).collect();
+        let thresh_res: Result<SegwitMiniScript, _> =
+            Concrete::Threshold(keys.len() - 1, keys).compile();
+        let ops_count = thresh_res.clone().and_then(|m| Ok(m.ext.ops_count_sat));
+        assert_eq!(
+            thresh_res,
+            Err(CompilerError::LimitsExceeded),
+            "Compilation succeeded with '{:?}' OP count (sat)",
+            ops_count,
+        );
+        // For legacy too..
+        let (keys, _) = pubkeys_and_a_sig(68);
+        let keys: Vec<Concrete<bitcoin::PublicKey>> =
+            keys.iter().map(|pubkey| Concrete::Key(*pubkey)).collect();
+        let thresh_res = Concrete::Threshold(keys.len() - 1, keys).compile::<Legacy>();
+        let ops_count = thresh_res.clone().and_then(|m| Ok(m.ext.ops_count_sat));
+        assert_eq!(
+            thresh_res,
+            Err(CompilerError::LimitsExceeded),
+            "Compilation succeeded with '{:?}' OP count (sat)",
+            ops_count,
+        );
+
+        // Test that we refuse to compile policies with duplicated keys
+        let (keys, _) = pubkeys_and_a_sig(1);
+        let key = Concrete::Key(keys[0]);
+        let res = Concrete::Or(vec![(1, key.clone()), (1, key.clone())]).compile::<Segwitv0>();
+        assert_eq!(
+            res,
+            Err(CompilerError::PolicyError(
+                policy::concrete::PolicyError::DuplicatePubKeys
+            ))
+        );
+        // Same for legacy
+        let res = Concrete::Or(vec![(1, key.clone()), (1, key)]).compile::<Legacy>();
+        assert_eq!(
+            res,
+            Err(CompilerError::PolicyError(
+                policy::concrete::PolicyError::DuplicatePubKeys
+            ))
         );
     }
 }
