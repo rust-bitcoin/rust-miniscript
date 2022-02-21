02d1702ec3c25e4652c2661176e9da56da0222bc
Jeremy Rubin <j@rubin.io>
2022-02-21 15:13:21 -0800

    Fix PSBT Code

--- src/psbt/mod.rs
+++ src/psbt/mod.rs
@@ -916,6 +916,7 @@ pub enum SigHashError {
     MissingRedeemScript,
 }
 
+<<<<<<< HEAD
 impl fmt::Display for SigHashError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -930,6 +931,10 @@ impl fmt::Display for SigHashError {
             }
             SigHashError::MissingWitnessScript => write!(f, "Missing Witness Script"),
             SigHashError::MissingRedeemScript => write!(f, "Missing Redeem Script"),
+=======
+        if let Some(witness) = input.final_script_witness.as_ref() {
+            ret.input[n].witness = witness.clone();
+>>>>>>> a5a2e87 (Fix PSBT Code)
         }
     }
 }
