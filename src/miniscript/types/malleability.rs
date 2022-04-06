52d66477274e9afa770fdb61f53ec8b13f68d750
Aman Rojjha <aman.rojjha@research.iiit.ac.in>
2022-04-07 02:30:55 +0530

    Add SigType check in CompilerExtData for deciding key byte-lengths

--- src/miniscript/types/malleability.rs
+++ src/miniscript/types/malleability.rs
@@ -15,6 +15,7 @@
 //! Malleability-related Type properties
 
 use super::{ErrorKind, Property};
+use crate::ScriptContext;
 
 /// Whether the fragment has a dissatisfaction, and if so, whether
 /// it is unique. Affects both correctness and malleability-freeness,
@@ -94,7 +95,7 @@ impl Property for Malleability {
         }
     }
 
-    fn from_pk_k() -> Self {
+    fn from_pk_k<Ctx: ScriptContext>() -> Self {
         Malleability {
             dissat: Dissat::Unique,
             safe: true,
@@ -102,7 +103,7 @@ impl Property for Malleability {
         }
     }
 
-    fn from_pk_h() -> Self {
+    fn from_pk_h<Ctx: ScriptContext>() -> Self {
         Malleability {
             dissat: Dissat::Unique,
             safe: true,
