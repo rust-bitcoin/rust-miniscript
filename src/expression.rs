ea57d6e1bc4b4fa778da347a9d8cf27d039fe32a
Tobin C. Harding <me@tobin.cc>
2022-04-22 13:42:13 +1000

    Use contains instead of manual implementation
    Clippy emits:
    
      warning: manual `!RangeInclusive::contains` implementation
    
    As suggested, use contains instead of manual implementation.

--- src/expression.rs
+++ src/expression.rs
@@ -195,7 +195,7 @@ impl<'a> Tree<'a> {
 pub fn parse_num(s: &str) -> Result<u32, Error> {
     if s.len() > 1 {
         let ch = s.chars().next().unwrap();
-        if ch < '1' || ch > '9' {
+        if !('1'..='9').contains(&ch) {
             return Err(Error::Unexpected(
                 "Number must start with a digit 1-9".to_string(),
             ));
