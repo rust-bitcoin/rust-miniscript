b8097106f8dfd31032d7894fb9b4c63dda31e0e4
Tobin C. Harding <me@tobin.cc>
2022-05-03 16:10:18 +1000

    examples: Refactor sign_multisig
    In an effort to make the example more clear; refactor the
    `sign_multisig` example.
    
    Refactor only, no logic changes.

--- examples/sign_multisig.rs
+++ examples/sign_multisig.rs
@@ -12,23 +12,87 @@
 // If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 //
 
-//! Example: Signing a 2-of-3 multisignature
+//! Example: Signing a 2-of-3 multisignature.
 
 use bitcoin;
 use miniscript;
 
 use bitcoin::blockdata::witness::Witness;
-use bitcoin::secp256k1; // secp256k1 re-exported from rust-bitcoin
+use bitcoin::secp256k1;
 use miniscript::DescriptorTrait;
 use std::collections::HashMap;
 use std::str::FromStr;
 
 fn main() {
-    // Avoid repeatedly typing a pretty-common descriptor type
-    type BitcoinDescriptor = miniscript::Descriptor<bitcoin::PublicKey>;
+    let mut tx = spending_transaction();
+    let pks = list_of_three_arbitrary_public_keys();
+    let sig = random_signature_from_the_blockchain();
 
-    // Transaction which spends some output
-    let mut tx = bitcoin::Transaction {
+    // Descriptor for the output being spent.
+    let s = format!("wsh(multi(2,{},{},{}))", pks[0], pks[1], pks[2],);
+    let descriptor = miniscript::Descriptor::<bitcoin::PublicKey>::from_str(&s).unwrap();
+
+    // Check weight for witness satisfaction cost ahead of time.
+    // 4 (scriptSig length of 0) + 1 (witness stack size) + 106 (serialized witnessScript)
+    // + 73*2 (signature length + signatures + sighash bytes) + 1 (dummy byte) = 258
+    assert_eq!(descriptor.max_satisfaction_weight().unwrap(), 258);
+
+    // Sometimes it is necessary to have additional information to get the
+    // `bitcoin::PublicKey` from the `MiniscriptKey` which can be supplied by
+    // the `to_pk_ctx` parameter. For example, when calculating the script
+    // pubkey of a descriptor with xpubs, the secp context and child information
+    // maybe required.
+
+    // Observe the script properties, just for fun.
+    assert_eq!(
+        format!("{:x}", descriptor.script_pubkey()),
+        "00200ed49b334a12c37f3df8a2974ad91ff95029215a2b53f78155be737907f06163"
+    );
+
+    assert_eq!(
+        format!(
+            "{:x}",
+            descriptor
+                .explicit_script()
+                .expect("wsh descriptors have unique inner script")
+        ),
+        "52\
+         21020202020202020202020202020202020202020202020202020202020202020202\
+         21020102030405060708010203040506070801020304050607080000000000000000\
+         21030102030405060708010203040506070801020304050607080000000000000000\
+         53ae"
+    );
+
+    // Attempt to satisfy at age 0, height 0.
+    let original_txin = tx.input[0].clone();
+
+    let mut sigs = HashMap::<bitcoin::PublicKey, miniscript::bitcoin::EcdsaSig>::new();
+
+    // Doesn't work with no signatures.
+    assert!(descriptor.satisfy(&mut tx.input[0], &sigs).is_err());
+    assert_eq!(tx.input[0], original_txin);
+
+    // ...or one signature...
+    sigs.insert(pks[1], sig);
+    assert!(descriptor.satisfy(&mut tx.input[0], &sigs).is_err());
+    assert_eq!(tx.input[0], original_txin);
+
+    // ...but two signatures is ok.
+    sigs.insert(pks[2], sig);
+    assert!(descriptor.satisfy(&mut tx.input[0], &sigs).is_ok());
+    assert_ne!(tx.input[0], original_txin);
+    assert_eq!(tx.input[0].witness.len(), 4); // 0, sig, sig, witness script
+
+    // ...and even if we give it a third signature, only two are used.
+    sigs.insert(pks[0], sig);
+    assert!(descriptor.satisfy(&mut tx.input[0], &sigs).is_ok());
+    assert_ne!(tx.input[0], original_txin);
+    assert_eq!(tx.input[0].witness.len(), 4); // 0, sig, sig, witness script
+}
+
+// Transaction which spends some output.
+fn spending_transaction() -> bitcoin::Transaction {
+    bitcoin::Transaction {
         version: 2,
         lock_time: 0,
         input: vec![bitcoin::TxIn {
@@ -41,10 +105,12 @@ fn main() {
             script_pubkey: bitcoin::Script::new(),
             value: 100_000_000,
         }],
-    };
+    }
+}
 
+fn list_of_three_arbitrary_public_keys() -> Vec<bitcoin::PublicKey> {
     #[cfg_attr(feature="cargo-fmt", rustfmt_skip)]
-    let public_keys = vec![
+    vec![
         bitcoin::PublicKey::from_slice(&[2; 33]).expect("key 1"),
         bitcoin::PublicKey::from_slice(&[
             0x02,
@@ -60,10 +126,13 @@ fn main() {
             0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         ]).expect("key 3"),
-    ];
-    let bitcoin_sig = bitcoin::EcdsaSig {
-        // copied at random off the blockchain; this is not actually a valid
-        // signature for this transaction; Miniscript does not verify
+    ]
+}
+
+// Returns a signature copied at random off the blockchain; this is not actually
+// a valid signature for this transaction; Miniscript does not verify the validity.
+fn random_signature_from_the_blockchain() -> bitcoin::EcdsaSig {
+    bitcoin::EcdsaSig {
         sig: secp256k1::ecdsa::Signature::from_str(
             "3045\
              0221\
@@ -73,70 +142,5 @@ fn main() {
         )
         .unwrap(),
         hash_ty: bitcoin::EcdsaSighashType::All,
-    };
-
-    let descriptor_str = format!(
-        "wsh(multi(2,{},{},{}))",
-        public_keys[0], public_keys[1], public_keys[2],
-    );
-
-    // Descriptor for the output being spent
-    let my_descriptor =
-        BitcoinDescriptor::from_str(&descriptor_str[..]).expect("parse descriptor string");
-
-    // Check weight for witness satisfaction cost ahead of time.
-    // 4(scriptSig length of 0) + 1(witness stack size) + 106(serialized witnessScript)
-    // + 73*2(signature length + signatures + sighash bytes) + 1(dummy byte) = 258
-    assert_eq!(my_descriptor.max_satisfaction_weight().unwrap(), 258);
-
-    // Sometimes it is necessary to have additional information to get the bitcoin::PublicKey
-    // from the MiniscriptKey which can supplied by `to_pk_ctx` parameter. For example,
-    // when calculating the script pubkey of a descriptor with xpubs, the secp context and
-    // child information maybe required.
-
-    // Observe the script properties, just for fun
-    assert_eq!(
-        format!("{:x}", my_descriptor.script_pubkey()),
-        "00200ed49b334a12c37f3df8a2974ad91ff95029215a2b53f78155be737907f06163"
-    );
-
-    assert_eq!(
-        format!(
-            "{:x}",
-            my_descriptor
-                .explicit_script()
-                .expect("wsh descriptors have unique inner script")
-        ),
-        "52\
-         21020202020202020202020202020202020202020202020202020202020202020202\
-         21020102030405060708010203040506070801020304050607080000000000000000\
-         21030102030405060708010203040506070801020304050607080000000000000000\
-         53ae"
-    );
-
-    // Attempt to satisfy at age 0, height 0
-    let original_txin = tx.input[0].clone();
-
-    let mut sigs = HashMap::<bitcoin::PublicKey, miniscript::bitcoin::EcdsaSig>::new();
-
-    // Doesn't work with no signatures
-    assert!(my_descriptor.satisfy(&mut tx.input[0], &sigs).is_err());
-    assert_eq!(tx.input[0], original_txin);
-
-    // ...or one signature...
-    sigs.insert(public_keys[1], bitcoin_sig);
-    assert!(my_descriptor.satisfy(&mut tx.input[0], &sigs).is_err());
-    assert_eq!(tx.input[0], original_txin);
-
-    // ...but two signatures is ok
-    sigs.insert(public_keys[2], bitcoin_sig);
-    assert!(my_descriptor.satisfy(&mut tx.input[0], &sigs).is_ok());
-    assert_ne!(tx.input[0], original_txin);
-    assert_eq!(tx.input[0].witness.len(), 4); // 0, sig, sig, witness script
-
-    // ...and even if we give it a third signature, only two are used
-    sigs.insert(public_keys[0], bitcoin_sig);
-    assert!(my_descriptor.satisfy(&mut tx.input[0], &sigs).is_ok());
-    assert_ne!(tx.input[0], original_txin);
-    assert_eq!(tx.input[0].witness.len(), 4); // 0, sig, sig, witness script
+    }
 }
