1198be7a26258e3ec51596eef3038240a46ca733
Tobin C. Harding <me@tobin.cc>
2022-05-03 15:21:48 +1000

    examples: Refactor xpub_descriptors
    In an effort to make the example more clear refactor the main function
    into two separate functions.
    
    This is an example not a test; remove the assertion on creating the
    address with xpubs passed in the opposite order, instead include a
    comment explaining as such.

--- examples/xpub_descriptors.rs
+++ examples/xpub_descriptors.rs
@@ -12,55 +12,63 @@
 // If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 //
 
-//! Example: Parsing a xpub and getting address
-
-use miniscript::bitcoin::{self, secp256k1};
-use miniscript::{Descriptor, DescriptorPublicKey, DescriptorTrait, TranslatePk2};
+//! Example: Parsing a xpub and getting an address.
 
 use std::str::FromStr;
-fn main() {
-    // For deriving from descriptors, we need to provide a secp context
-    let secp_ctx = secp256k1::Secp256k1::verification_only();
-    // P2WSH and single xpubs
-    let addr_one = Descriptor::<DescriptorPublicKey>::from_str(
-            "wsh(sortedmulti(1,xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB,xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH))",
-        )
-        .unwrap()
-        .translate_pk2(|xpk| xpk.derive_public_key(&secp_ctx))
-        .unwrap()
-        .address(bitcoin::Network::Bitcoin).unwrap();
 
-    let addr_two = Descriptor::<DescriptorPublicKey>::from_str(
-            "wsh(sortedmulti(1,xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH,xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB))",
-        )
+use miniscript::bitcoin::secp256k1::{Secp256k1, Verification};
+use miniscript::bitcoin::{Address, Network};
+use miniscript::{Descriptor, DescriptorPublicKey, DescriptorTrait, TranslatePk2};
+
+const XPUB_1: &str = "xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB";
+const XPUB_2: &str = "xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH";
+
+fn main() {
+    // For deriving from descriptors, we need to provide a secp context.
+    let secp = Secp256k1::verification_only();
+
+    // P2WSH and single xpubs.
+    let _ = p2wsh(&secp);
+
+    // P2WSH-P2SH and ranged xpubs.
+    let _ = p2sh_p2wsh(&secp);
+}
+
+/// Parses a P2WSH descriptor, returns the associated address.
+fn p2wsh<C: Verification>(secp: &Secp256k1<C>) -> Address {
+    // It does not matter what order the two xpubs go in, the same address will be generated.
+    let s = format!("wsh(sortedmulti(1,{},{}))", XPUB_1, XPUB_2);
+    // let s = format!("wsh(sortedmulti(1,{},{}))", XPUB_2, XPUB_1);
+
+    let address = Descriptor::<DescriptorPublicKey>::from_str(&s)
         .unwrap()
-        .translate_pk2(|xpk| xpk.derive_public_key(&secp_ctx))
+        .translate_pk2(|xpk| xpk.derive_public_key(secp))
         .unwrap()
-        .address(bitcoin::Network::Bitcoin).unwrap();
+        .address(Network::Bitcoin)
+        .unwrap();
+
     let expected = bitcoin::Address::from_str(
         "bc1qpq2cfgz5lktxzr5zqv7nrzz46hsvq3492ump9pz8rzcl8wqtwqcspx5y6a",
     )
     .unwrap();
-    assert_eq!(addr_one, expected);
-    assert_eq!(addr_two, expected);
+    assert_eq!(address, expected);
+    address
+}
 
-    // P2WSH-P2SH and ranged xpubs
-    let addr_one = Descriptor::<DescriptorPublicKey>::from_str(
-            "sh(wsh(sortedmulti(1,xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB/1/0/*,xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH/0/0/*)))",
-        )
-        .unwrap()
-        .derived_descriptor(&secp_ctx, 5)
-        .unwrap()
-        .address(bitcoin::Network::Bitcoin).unwrap();
+/// Parses a P2SH-P2WSH descriptor, returns the associated address.
+fn p2sh_p2wsh<C: Verification>(secp: &Secp256k1<C>) -> Address {
+    // It does not matter what order the two xpubs go in, the same address will be generated.
+    let s = format!("sh(wsh(sortedmulti(1,{}/1/0/*,{}/0/0/*)))", XPUB_1, XPUB_2);
+    // let s = format!("sh(wsh(sortedmulti(1,{}/1/0/*,{}/0/0/*)))", XPUB_2, XPUB_1);
 
-    let addr_two = Descriptor::<DescriptorPublicKey>::from_str(
-            "sh(wsh(sortedmulti(1,xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH/0/0/*,xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB/1/0/*)))",
-        )
+    let address = Descriptor::<DescriptorPublicKey>::from_str(&s)
         .unwrap()
-        .derived_descriptor(&secp_ctx, 5)
+        .derived_descriptor(&secp, 5)
         .unwrap()
-        .address(bitcoin::Network::Bitcoin).unwrap();
-    let expected = bitcoin::Address::from_str("325zcVBN5o2eqqqtGwPjmtDd8dJRyYP82s").unwrap();
-    assert_eq!(addr_one, expected);
-    assert_eq!(addr_two, expected);
+        .address(Network::Bitcoin)
+        .unwrap();
+
+    let expected = Address::from_str("325zcVBN5o2eqqqtGwPjmtDd8dJRyYP82s").unwrap();
+    assert_eq!(address, expected);
+    address
 }
