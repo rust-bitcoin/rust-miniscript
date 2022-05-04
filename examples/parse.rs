c79235a020b16503400d55cd918b5d1b40d28e87
Tobin C. Harding <me@tobin.cc>
2022-05-04 12:00:08 +1000

    examples: Refactor parse
    In an effort to make the example more clear; refactor the
    `parse` example.
    
    Refactor only, no logic changes.

--- examples/parse.rs
+++ examples/parse.rs
@@ -12,7 +12,7 @@
 // If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 //
 
-//! Example: Parsing a descriptor from a string
+//! Example: Parsing a descriptor from a string.
 
 use bitcoin;
 use miniscript;
@@ -21,54 +21,49 @@ use miniscript::{descriptor::DescriptorType, Descriptor, DescriptorTrait};
 use std::str::FromStr;
 
 fn main() {
-    let my_descriptor = miniscript::Descriptor::<bitcoin::PublicKey>::from_str(
+    let desc = miniscript::Descriptor::<bitcoin::PublicKey>::from_str(
         "wsh(c:pk_k(020202020202020202020202020202020202020202020202020202020202020202))",
     )
     .unwrap();
 
-    // Check whether the descriptor is safe
-    // This checks whether all spend paths are accessible in bitcoin network.
-    // It maybe possible that some of the spend require more than 100 elements in Wsh scripts
-    // Or they contain a combination of timelock and heightlock.
-    assert!(my_descriptor.sanity_check().is_ok());
+    // Check whether the descriptor is safe. This checks whether all spend paths are accessible in
+    // the Bitcoin network. It may be possible that some of the spend paths require more than 100
+    // elements in Wsh scripts or they contain a combination of timelock and heightlock.
+    assert!(desc.sanity_check().is_ok());
 
     // Compute the script pubkey. As mentioned in the documentation, script_pubkey only fails
-    // for Tr descriptors that don't have some pre-computed data
+    // for Tr descriptors that don't have some pre-computed data.
     assert_eq!(
-        format!("{:x}", my_descriptor.script_pubkey()),
+        format!("{:x}", desc.script_pubkey()),
         "0020daef16dd7c946a3e735a6e43310cb2ce33dfd14a04f76bf8241a16654cb2f0f9"
     );
 
-    // Another way to compute script pubkey
-    // We can also compute the type of descriptor
-    let desc_type = my_descriptor.desc_type();
+    // As another way to compute script pubkey; we can also compute the type of the descriptor.
+    let desc_type = desc.desc_type();
     assert_eq!(desc_type, DescriptorType::Wsh);
-    // Since we know the type of descriptor, we can get the Wsh struct from Descriptor
-    // This allows us to call infallible methods for getting script pubkey
-    if let Descriptor::Wsh(wsh) = &my_descriptor {
+    // Since we know the type of descriptor, we can get the Wsh struct from Descriptor. This allows
+    // us to call infallible methods for getting script pubkey.
+    if let Descriptor::Wsh(wsh) = &desc {
         assert_eq!(
             format!("{:x}", wsh.spk()),
             "0020daef16dd7c946a3e735a6e43310cb2ce33dfd14a04f76bf8241a16654cb2f0f9"
         );
-    } else {
-        // We checked for the descriptor type earlier
     }
 
-    // Get the inner script inside the descriptor
+    // Get the inner script inside the descriptor.
     assert_eq!(
         format!(
             "{:x}",
-            my_descriptor
-                .explicit_script()
+            desc.explicit_script()
                 .expect("Wsh descriptors have inner scripts")
         ),
         "21020202020202020202020202020202020202020202020202020202020202020202ac"
     );
 
+    // In a similar fashion we can parse a wrapped segwit script.
     let desc = miniscript::Descriptor::<bitcoin::PublicKey>::from_str(
         "sh(wsh(c:pk_k(020202020202020202020202020202020202020202020202020202020202020202)))",
     )
     .unwrap();
-
     assert!(desc.desc_type() == DescriptorType::ShWsh);
 }
