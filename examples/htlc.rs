a836bcb85fb5f5de082fa401704a22536117fb02
Tobin C. Harding <me@tobin.cc>
2023-07-03 11:23:31 +1000

    examples: Use SPDX licence identifiers
    As we do in the main source directory; use SPDX license identifiers in
    all of the examples files. The SPDX license is a balance between minimal
    noise and having license coverage for those who want/like/need it.
    
    Note, removes the "written by" section if it uses Andrews name because
    he has stated elsewhere that this is ok. Leaves Thomas' attribution in
    there.

--- examples/htlc.rs
+++ examples/htlc.rs
@@ -1,16 +1,5 @@
-// Miniscript
-// Written in 2019 by
-//    Thomas Eizinger <thomas@coblox.tech>
-//
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to
-// the public domain worldwide. This software is distributed without
-// any warranty.
-//
-// You should have received a copy of the CC0 Public Domain Dedication
-// along with this software.
-// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//
+// Written by Thomas Eizinger <thomas@coblox.tech>
+// SPDX-License-Identifier: CC0-1.0
 
 //! Example: Create an HTLC with miniscript using the policy compiler
 
--- examples/taproot.rs
+++ examples/taproot.rs
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: CC0-1.0
+
 use std::collections::HashMap;
 use std::str::FromStr;
 
