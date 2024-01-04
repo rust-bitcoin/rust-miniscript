3b47c3cf1b22ac0432d1e1191250a6a9daf55688
Bruno Garcia <brunoely.gc@gmail.com>
2024-01-04 10:57:25 -0300

    fuzz: cover taproot in `roundtrip_miniscript_str`

--- fuzz/fuzz_targets/roundtrip_miniscript_str.rs
+++ fuzz/fuzz_targets/roundtrip_miniscript_str.rs
@@ -1,13 +1,18 @@
 use std::str::FromStr;
 
 use honggfuzz::fuzz;
-use miniscript::{Miniscript, Segwitv0};
+use miniscript::{Miniscript, Segwitv0, Tap};
 
 fn do_test(data: &[u8]) {
     let s = String::from_utf8_lossy(data);
     if let Ok(desc) = Miniscript::<String, Segwitv0>::from_str(&s) {
         let str2 = desc.to_string();
         let desc2 = Miniscript::<String, Segwitv0>::from_str(&str2).unwrap();
+
+        assert_eq!(desc, desc2);
+    } else if let Ok(desc) = Miniscript::<String, Tap>::from_str(&s) {
+        let str2 = desc.to_string();
+        let desc2: Miniscript<String, Tap> = Miniscript::<String, Tap>::from_str(&str2).unwrap();
 
         assert_eq!(desc, desc2);
     }
