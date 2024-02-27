3725549e92fdaee24a23e1c96ada1e4c3a33e60c
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-02-27 15:00:38 +0000

    ci: tighten import of `Vec`

--- src/policy/mod.rs
+++ src/policy/mod.rs
@@ -23,7 +23,9 @@ pub use self::semantic::Policy as Semantic;
 use crate::descriptor::Descriptor;
 use crate::miniscript::{Miniscript, ScriptContext};
 use crate::sync::Arc;
-use crate::{Error, MiniscriptKey, Terminal, Vec};
+use crate::{Error, MiniscriptKey, Terminal};
+#[cfg(all(not(feature = "std"), not(test)))]
+use crate::Vec;
 
 /// Policy entailment algorithm maximum number of terminals allowed.
 const ENTAILMENT_MAX_TERMINALS: usize = 20;
