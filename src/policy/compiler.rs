6d663e15cee0586579fd5ef00812b3875b6ae382
Tobin C. Harding <me@tobin.cc>
2024-03-28 11:23:21 +1100

    Allow range loop in test code
    The explicit array access is clear, just shoosh the linter.

--- src/policy/compiler.rs
+++ src/policy/compiler.rs
@@ -1351,6 +1351,7 @@ mod tests {
     }
 
     #[test]
+    #[allow(clippy::needless_range_loop)]
     fn compile_misc() {
         let (keys, sig) = pubkeys_and_a_sig(10);
         let key_pol: Vec<BPolicy> = keys.iter().map(|k| Concrete::Key(*k)).collect();
