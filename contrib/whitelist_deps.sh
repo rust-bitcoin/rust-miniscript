2e37b34b1b732be6db8eafbf4196f37a148b25aa
Tobin C. Harding <me@tobin.cc>
2024-05-02 15:35:14 +1000

    CI: Use script from rust-bitcoin-maintainer-tools
    We have a CI script in the `rust-bitcoin-maintainer-tools` repository,
    lets use it.

---
+++ contrib/whitelist_deps.sh
@@ -0,0 +1,8 @@
+# No shebang, this file should not be executed.
+# shellcheck disable=SC2148
+#
+# disable verify unused vars, despite the fact that they are used when sourced
+# shellcheck disable=SC2034
+
+# Remove once we upgrade to `bitcoin v0.32.0`.
+DUPLICATE_DEPS=("bech32")
