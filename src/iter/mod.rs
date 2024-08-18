d0c0c14ae26e353c950319182fd853ee90b8eb54
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-08-18 21:34:13 +0000

    miniscript: nonrecursive implementation of PartialEq/Eq/Hash

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -76,3 +76,33 @@ impl<'a, Pk: MiniscriptKey, Ctx: ScriptContext> TreeLike for &'a Arc<Miniscript<
         }
     }
 }
+
+impl<'a, Pk: MiniscriptKey, Ctx: ScriptContext> TreeLike for &'a Terminal<Pk, Ctx> {
+    type NaryChildren = &'a [Arc<Miniscript<Pk, Ctx>>];
+
+    fn nary_len(tc: &Self::NaryChildren) -> usize { tc.len() }
+    fn nary_index(tc: Self::NaryChildren, idx: usize) -> Self { tc[idx].as_inner() }
+
+    fn as_node(&self) -> Tree<Self, Self::NaryChildren> {
+        use Terminal::*;
+        match self {
+            PkK(..) | PkH(..) | RawPkH(..) | After(..) | Older(..) | Sha256(..) | Hash256(..)
+            | Ripemd160(..) | Hash160(..) | True | False | Multi(..) | MultiA(..) => Tree::Nullary,
+            Alt(ref sub)
+            | Swap(ref sub)
+            | Check(ref sub)
+            | DupIf(ref sub)
+            | Verify(ref sub)
+            | NonZero(ref sub)
+            | ZeroNotEqual(ref sub) => Tree::Unary(sub.as_inner()),
+            AndV(ref left, ref right)
+            | AndB(ref left, ref right)
+            | OrB(ref left, ref right)
+            | OrD(ref left, ref right)
+            | OrC(ref left, ref right)
+            | OrI(ref left, ref right) => Tree::Binary(left.as_inner(), right.as_inner()),
+            AndOr(ref a, ref b, ref c) => Tree::Ternary(a.as_inner(), b.as_inner(), c.as_inner()),
+            Thresh(ref thresh) => Tree::Nary(thresh.data()),
+        }
+    }
+}
