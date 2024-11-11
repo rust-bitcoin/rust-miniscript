7e1c1baeebd1ed8da68d5729eaf3befc44f0ba4b
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-11-11 16:53:36 +0000

    ci: add `taptree_of_horror` to list of examples to run

--- contrib/test_vars.sh
+++ contrib/test_vars.sh
@@ -12,4 +12,4 @@ FEATURES_WITHOUT_STD="compiler trace serde rand base64"
 
 # Run these examples.
 # Note `examples/big` should not be run.
-EXAMPLES="htlc:std,compiler parse:std sign_multisig:std verify_tx:std xpub_descriptors:std taproot:std,compiler psbt_sign_finalize:std,base64"
+EXAMPLES="htlc:std,compiler parse:std sign_multisig:std verify_tx:std xpub_descriptors:std taproot:std,compiler psbt_sign_finalize:std,base64 taptree_of_horror:std,compiler"
