d1dadc607c06daccbb35b3362a2d78a80bc909fc
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-03-30 20:19:16 +0000

    fuzz: add regression test for taptree behavior
    This fuzztest currently does almost nothing, because both 12.x and
    master just pass through to rust-bitcoin's `TapRootSpendInfo` structure
    for everything related to taptrees.
    
    In a later PR we will move all this stuff into rust-miniscript and then
    we will want a regression test.

--- fuzz/src/lib.rs
+++ fuzz/src/lib.rs
@@ -42,13 +42,6 @@ impl MiniscriptKey for FuzzPk {
     type Hash256 = u8;
 }
 
-impl old_miniscript::MiniscriptKey for FuzzPk {
-    type Sha256 = u8;
-    type Ripemd160 = u8;
-    type Hash160 = u8;
-    type Hash256 = u8;
-}
-
 impl ToPublicKey for FuzzPk {
     fn to_public_key(&self) -> PublicKey {
         let secp_pk = secp256k1::PublicKey::from_slice(&[
@@ -66,6 +59,41 @@ impl ToPublicKey for FuzzPk {
 
     fn to_hash256(hash: &Self::Hash256) -> hash256::Hash {
         hash256::Hash::from_byte_array([*hash; 32])
+    }
+
+    fn to_ripemd160(hash: &Self::Ripemd160) -> ripemd160::Hash {
+        ripemd160::Hash::from_byte_array([*hash; 20])
+    }
+
+    fn to_hash160(hash: &Self::Ripemd160) -> hash160::Hash {
+        hash160::Hash::from_byte_array([*hash; 20])
+    }
+}
+
+impl old_miniscript::MiniscriptKey for FuzzPk {
+    type Sha256 = u8;
+    type Ripemd160 = u8;
+    type Hash160 = u8;
+    type Hash256 = u8;
+}
+
+impl old_miniscript::ToPublicKey for FuzzPk {
+    fn to_public_key(&self) -> PublicKey {
+        let secp_pk = secp256k1::PublicKey::from_slice(&[
+            0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3b, 0x78,
+            0xce, 0x56, 0x3f, 0x89, 0xa0, 0xed, 0x94, 0x14, 0xf5, 0xaa, 0x28, 0xad, 0x0d, 0x96,
+            0xd6, 0x79, 0x5f, 0x9c, 0x63, 0x3f, 0x39, 0x79, 0xbf, 0x72, 0xae, 0x82, 0x02, 0x98,
+            0x3d, 0xc9, 0x89, 0xae, 0xc7, 0xf2, 0xff, 0x2e, 0xd9, 0x1b, 0xdd, 0x69, 0xce, 0x02,
+            0xfc, 0x07, 0x00, 0xca, 0x10, 0x0e, 0x59, 0xdd, 0xf3,
+        ])
+        .unwrap();
+        PublicKey { inner: secp_pk, compressed: self.compressed }
+    }
+
+    fn to_sha256(hash: &Self::Sha256) -> sha256::Hash { sha256::Hash::from_byte_array([*hash; 32]) }
+
+    fn to_hash256(hash: &Self::Hash256) -> old_miniscript::hash256::Hash {
+        old_miniscript::hash256::Hash::from_byte_array([*hash; 32])
     }
 
     fn to_ripemd160(hash: &Self::Ripemd160) -> ripemd160::Hash {
