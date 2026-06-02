69be31bf206952dcbe68f3a248c153c29df6b2cf
nymius <155548262+nymius@users.noreply.github.com>
2026-06-02 10:15:23 -0300

    feat: expose `Plan.descriptor` field

--- src/plan.rs
+++ src/plan.rs
@@ -217,8 +217,8 @@ pub struct Plan<Pk: MiniscriptKey> {
     pub absolute_timelock: Option<absolute::LockTime>,
     /// The relative timelock this plan uses
     pub relative_timelock: Option<relative::LockTime>,
-
-    pub(crate) descriptor: Descriptor<Pk>,
+    /// The target descriptor for this plan
+    pub descriptor: Descriptor<Pk>,
 }
 
 impl<Pk: MiniscriptKey + ToPublicKey> Plan<Pk> {
