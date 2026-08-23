1a80c39a83b61a1c3b11e1da4172e36598824f58
Andrew Poelstra <apoelstra@wpsoftware.net>
2026-08-23 18:10:48 +0000

    export primitives::positive_f64::NormalizedIterator
    It feels a little weird to export this at the root but there isn't
    anywhere else in the public API where it really fits, so just dump it
    there.

--- src/lib.rs
+++ src/lib.rs
@@ -141,7 +141,7 @@ pub use crate::miniscript::{hash256, Miniscript};
 pub use crate::policy::semantic::MathSyntaxError;
 use crate::prelude::*;
 pub use crate::primitives::absolute_locktime::{AbsLockTime, AbsLockTimeError};
-pub use crate::primitives::positive_f64::PositiveF64;
+pub use crate::primitives::positive_f64::{NormalizedIterator, PositiveF64};
 pub use crate::primitives::relative_locktime::{RelLockTime, RelLockTimeError};
 pub use crate::primitives::threshold::{Threshold, ThresholdError};
 pub use crate::validation::{Error as ValidationError, ValidationParams};
